1fca0d77336be81e1710c69ed2491ce7c6f82b5a
Clewerton Roque <clewerton203@gmail.com>
2021-02-17 19:45:05 -0300

    first commit

--- pages/sobre.js
+++ pages/sobre.js
@@ -2,11 +2,12 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 
+
 export default function Home() {
   return (
     <div className={styles.container}>
       <h1>Teste</h1>
-      <Link href='/sobre'>
+      <Link href='/'>
         <a>Acessar home</a>
       </Link>
     </div>
